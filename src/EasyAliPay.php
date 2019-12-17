b593cac1171cf97911d537f56590a8a5ca6847d0
flashytime <myflashytime@gmail.com>
2019-12-17 22:12:13 +0800

    init and first commit

---
+++ src/EasyAliPay.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * EasyAliPay.php
+ * This file is part of EasyAliPay.
+ * Author: flashytime <myflashytime@gmail.com>
+ * Date: 2019/12/17 22:11
+ */
+
+namespace EasyAliPay;
+
+class EasyAliPay
+{
+
+}
